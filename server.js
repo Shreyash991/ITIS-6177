f03753c3c939b08cb1c30bbe204dc60b5571815c
Shreyash Thakre <shreyashthakre1999@gmail.com>
2024-09-01 22:20:30 -0400

    Added Node.js server using Express

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const port = 3000;
 
-const requestHandler = (req, res) => {
-  res.end('Hello, World!');
-};
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-const server = http.createServer(requestHandler);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
